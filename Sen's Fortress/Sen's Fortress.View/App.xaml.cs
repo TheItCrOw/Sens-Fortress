5882cb01e1ff7404543107648d535b48409d8e24
Kevin Bönisch <k.boenisch@outlook.com>
2019-10-06 10:49:43 +0200

    Readjusted Soution

--- Sen's Fortress/Sen's Fortress.View/App.xaml.cs
+++ Sen's Fortress/Sen's Fortress.View/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Sen_s_Fortress
+namespace Sen_s_Fortress.View
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Sen's Fortress/Sen's Fortress.View/MainWindow.xaml.cs
+++ Sen's Fortress/Sen's Fortress.View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Sen_s_Fortress
+namespace Sen_s_Fortress.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
