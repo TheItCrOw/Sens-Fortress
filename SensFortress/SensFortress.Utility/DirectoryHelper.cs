a36b73c4cb5d6f142f6f0f9e71b64615670a966a
Kevin Bönisch <k.boenisch@outlook.com>
2019-10-07 16:11:17 +0200

    Rep Push again

---
+++ SensFortress/SensFortress.Utility/DirectoryHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SensFortress.Utility
+{
+    /// <summary>
+    /// Class that creates and holds every necessesary directory.
+    /// </summary>
+    static class DirectoryHelper
+    {
+        /// <summary>
+        /// Creates and returns Directory for the <see cref="LogHelper"/>.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetLogDirectory()
+        {
+            var fullPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Sen's Fortress\\");
+            var createdDirectory = System.IO.Directory.CreateDirectory(fullPath);            
+            return fullPath + "Log.txt";
+        }
+
+    }
+}
