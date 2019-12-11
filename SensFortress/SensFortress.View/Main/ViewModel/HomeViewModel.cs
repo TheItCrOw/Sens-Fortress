1be3b2effd2a9ec795f636c1bfac2a90b7a7fc4a
Kevin Bönisch <k.boenisch@outlook.com>
2019-12-11 11:00:29 +0100

    PrismCore installed

---
+++ SensFortress/SensFortress.View/Main/ViewModel/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SensFortress.View.Main.ViewModel
+{
+    public class HomeViewModel
+    {
+
+        //=> testing
+
+        public ObservableCollection<string> TestCollection = new ObservableCollection<string>(); 
+
+        //<= testing
+
+    }
+}
