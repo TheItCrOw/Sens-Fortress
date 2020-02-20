3698d73652eaa07a3c4331431f40920394cfd508
unknown <keboen@web.de>
2020-02-20 14:31:49 +0100

    Testing

--- SensFortress/SensFortress.Utility/PDF.cs
+++ SensFortress/SensFortress.Utility/PDF.cs
@@ -1,5 +1,4 @@
-﻿using PdfSharp.Drawing;
-using PdfSharp.Pdf;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,15 +10,7 @@ namespace SensFortress.Utility
     {
         public static void CreateNew()
         {
-            PdfDocument pdf = new PdfDocument();
-            pdf.Info.Title = "Sen's fortress passwords.";
-            PdfPage pdfPage = pdf.AddPage();
-            XGraphics graph = XGraphics.FromPdfPage(pdfPage);
-            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
-            graph.DrawString("This is my first PDF document", font, XBrushes.Black, new XRect(0, 0, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.Center);
-            string pdfFilename = $"{IOPathHelper.GetDesktopPath()}\\Passwords.pdf";
-            pdf.Save(pdfFilename);
-            Process.Start(pdfFilename);
+
         }
 
     }
